be2ff724fda6353d57012d3be9ec4266d516d16d
Ben Mills <bengeorgemills@gmail.com>
2021-02-09 16:34:50 +0000

    First Upload

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const command = require('discord.js-commando');
 const Discord = require('discord.js');
 const bot = new command.Client();
 
-//Logging in to Server and stating when ready
-//testserver
-bot.login('NDcyNzQwMjcwNjY5MTY4NjQx.DlHc0Q.WNAjiVx4l-4rjA05Z1hmSBPIskY')
-
-//Adam's Server
-//bot.login('NDc3MTI0Mjk0ODA0MjQyNDQy.DlHang.MAC4KrJfGITYmvq93rBDGTtz4fI');
-
 bot.on('ready', function(){
 
   //A global variable used to tell if the bots in a voice channel
--- test.js
+++
@@ -1,5 +0,0 @@
-lines (17 sloc) 514 Bytes
-const OBSWebSocket = require('obs-websocket-js');
-const obs = new OBSWebSocket();
-
-obs.connect()
